4d6908d28de4315acf54ec344418e8de67b52856
Alen <alenhasana@yahoo.dk>
2019-10-20 10:08:40 +0200

    Made overall design of page
    Everything that should be in the screen is now in the screen

--- ios/Runner/GeneratedPluginRegistrant.m
+++ ios/Runner/GeneratedPluginRegistrant.m
@@ -3,10 +3,14 @@
 //
 
 #import "GeneratedPluginRegistrant.h"
+#import <system_shortcuts/SystemShortcutsPlugin.h>
+#import <volume/VolumePlugin.h>
 
 @implementation GeneratedPluginRegistrant
 
 + (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [SystemShortcutsPlugin registerWithRegistrar:[registry registrarForPlugin:@"SystemShortcutsPlugin"]];
+  [VolumePlugin registerWithRegistrar:[registry registrarForPlugin:@"VolumePlugin"]];
 }
 
 @end
