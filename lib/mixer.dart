406cdc4d0ce86561eb16549bcb294b2977ac4d3c
Alen <alenhasana@yahoo.dk>
2019-10-20 19:26:03 +0200

    Design is Made
    Design for the mixers are made, but not functionality

--- lib/mixer.dart
+++ lib/mixer.dart
@@ -1,7 +1,19 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class mixer extends StatelessWidget {
+class mixer extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return mixerState();
+  }
+
+}
+
+
+
+class mixerState extends State<mixer> {
+  double d = 0.0;
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
@@ -13,21 +25,53 @@ class mixer extends StatelessWidget {
         body: Container(
           child: Column(
             children: <Widget>[
-              Slider(
-                value: 1,
-              ),
-              Slider(
-                value: 1,
-              ),
-              Slider(
-                value: 1,
-              ),
-              Slider(
-                value: 1,
-              ),
+              SliderDesign("Volume", d),
+              SliderDesign("Filter", d),
+              SliderDesign("Bass", d),
+              SliderDesign("Noise", d),
             ],
           ),
           margin: EdgeInsets.fromLTRB(5, 20, 5, 10),
         ));
   }
+
+  Widget SliderDesign(String text, double value) {
+
+    return Container (
+        child: Column(children: <Widget>[
+     /*     Expanded(child: Text('Noise'),
+            flex: 1,),
+*/
+     Text(text),
+
+      SliderTheme(
+        data: SliderThemeData(
+            thumbColor: Colors.teal,
+            trackHeight: 10,
+            thumbShape: RoundSliderThumbShape(enabledThumbRadius: 15)),
+
+      child: Slider (
+        min: 0.0,
+        max: 5.0,
+        divisions: 5,
+        value: d,
+        onChanged: (double q) {
+          d = q;
+          setState(() {
+
+          });
+        },
+      ),
+    ),
+     // flex: 5,),
+  ]
+    ));
+
+
+  }
+
+
+
+
 }
+
