58a4e7545063bda66a828675f5a7007052611978
Burhan27 <s175446@student.dtu.dk>
2019-10-21 11:44:42 +0200

    Revert "Revert "Merge branch 'settings' into develop""
    This reverts commit e312bd118bce8f945e25d545ebf738ee1ebce8e4.

--- lib/router.dart
+++ lib/router.dart
@@ -1,6 +1,8 @@
+import 'package:audientes/mixer.dart';
 import 'package:flutter/material.dart';
 import 'startscreen.dart';
 import 'settings.dart';
+import 'mixer.dart';
 
 
 class Router{
@@ -11,6 +13,8 @@ class Router{
         return MaterialPageRoute(builder: (_) => MyApp());
       case '/settings':
         return MaterialPageRoute(builder: (_) => Settings());
+      case '/mixer':
+        return MaterialPageRoute(builder: (_) => mixer());
       default:
         return MaterialPageRoute(
             builder: (_) => Scaffold(
--- lib/settings.dart
+++ lib/settings.dart
@@ -1,3 +1,4 @@
+import 'package:audientes/settingOption.dart';
 import 'package:flutter/material.dart';
 
 class Settings extends StatelessWidget {
@@ -5,15 +6,38 @@ class Settings extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Second Route"),
-      ),
-      body: Center(
-        child: RaisedButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          child: Text('Go back!'),
+        backgroundColor: Color(0xff131313),
+        title: Text(
+          "Settings",
+          style: TextStyle(color: Colors.white, fontSize: 25),
         ),
+        actions: <Widget>[
+          GestureDetector(
+            child: Text(
+              '+',
+              style: TextStyle(fontSize: 20, color: Color(0xffFFB20A)),
+            ),
+          onTap: ()=> Navigator.pushNamed(context, '/mixer'),)
+        ],
+      ),
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            settingOption('Noisy'),
+            settingOption('TV'),
+            settingOption('Movie'),
+            settingOption('Outdoor'),
+            RaisedButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: Text('Back'),
+            )
+          ],
+        ),
+        width: double.infinity,
+        alignment: Alignment.center,
+        color: Color(0xff131313),
       ),
     );
   }
